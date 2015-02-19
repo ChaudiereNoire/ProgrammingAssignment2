28a1cdc0eaa2742ff46a4e155c349981406e6a70
Steven LaValle <steven.lavalle@gmail.com>
2015-02-18 21:55:58 -0500

    This first commit is to check out the process for completing the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file is produced as the gradded materal for Assignment
+## Two.
 
 ## Write a short comment describing this function
 
